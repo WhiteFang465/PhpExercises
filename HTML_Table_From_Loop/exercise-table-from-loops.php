2b21fcc2d688e4d1198f4efacb6445930bf59c5f
Atul Jairam <shadesofaura@gmail.com>
2022-02-25 23:50:53 -0500

    completed with exercise-table-from-loops.php

--- HTML_Table_From_Loop/exercise-table-from-loops.php
+++ HTML_Table_From_Loop/exercise-table-from-loops.php
@@ -1,18 +1,45 @@
 <?php
 
 
-const USERS = array(
-    array('Pierre', 22, '123 rue A', 'aa@ser.com', array('programming' => 5, 'teaching' => 2)),
-    array('Julie', 65, '123 rue B', 'bb@ser.com', array('electronics' => 46)),
-    array('Martin', 45, '123 rue C', 'cc@ser.com', array('programming' => 21, 'teaching' => 1)),
-    array('Melanie', 41, '123 rue D', 'dd@ser.com', array('welding' => 12, 'nutrition' => 6, 'restoration' => 1)),
-);
+const USERS = array(array('Pierre', 22, '123 rue A', 'aa@ser.com', array('programming' => 5, 'teaching' => 2)), array('Julie', 65, '123 rue B', 'bb@ser.com', array('electronics' => 46)), array('Martin', 45, '123 rue C', 'cc@ser.com', array('programming' => 21, 'teaching' => 1)), array('Melanie', 41, '123 rue D', 'dd@ser.com', array('welding' => 12, 'nutrition' => 6, 'restoration' => 1)),);
 
 // todo: Black color when age is exactly equal to reference age, green when upper and blue when lower
 const AGE_REFERENCE = 45;
 
 // todo: Yellow color when the years of experience is equal to or greater than MINIMUM_EXPERIENCE
 const MINIMUM_EXPERIENCE = 5;
+
+
+function getClassForAge(int $age): string
+{
+    switch ($age) {
+        case $age == AGE_REFERENCE:
+            return "age-reference ";
+            break;
+        case $age > AGE_REFERENCE:
+            return "age-over";
+            break;
+        case $age < AGE_REFERENCE:
+            return "age-under ";
+        default:
+            return " ";
+    }
+
+}
+
+function getClassForExperience(int $experience): string
+{
+    switch ($experience) {
+        case $experience >= MINIMUM_EXPERIENCE:
+            return "experience-valid";
+            break;
+        case $experience < MINIMUM_EXPERIENCE:
+            return "";
+            break;
+        default:
+            return "";
+    }
+}
 
 ?>
 
@@ -68,22 +95,67 @@ const MINIMUM_EXPERIENCE = 5;
 
 <table>
     <thead>
+
     <tr>
         <th>Name</th>
         <th>Age</th>
         <th>Address</th>
         <th>Email</th>
         <th>Jobs</th>
+
+
     </tr>
     </thead>
+    <?php
+    foreach (USERS as $USER) {
 
-    <tbody>
 
-    </tbody>
+        ?>
+        <tbody>
+        <tr class="<?php echo getClassForAge($USER[1]) ?>">
+            <td><?= $USER[0] ?></td>
+            <td><?= $USER[1] ?></td>
+            <td><?= $USER[2] ?></td>
+            <td><?= $USER[3] ?></td>
+            <td>
+
+                <?php
+                foreach ($USER[4] as $jobDescription => $jobExperience) {
+                    ?>
+                    <div class="<?= getClassForExperience($jobExperience) ?>">
+                        <?= $jobDescription . " : " . $jobExperience . " Years <br/>" ?>
+
+                    </div>
+                    <br>
+
+
+                    <?php
+                }
+                ?>
+            </td>
+        </tr>
+        </tbody>
+        <?php
+    }
+    ?>
 
     <tfoot>
+    <tr>
+        <td>Average</td>
+        <td><?php
 
+            $temp = 0;
+            foreach (USERS as $USER) {
+
+                $temp += intval($USER[1]);
+            }
+            echo $temp / count(USERS);
+            ?>
+
+        </td>
+    </tr>
     </tfoot>
+
 </table>
 
 </body>
