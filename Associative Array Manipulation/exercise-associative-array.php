d5dddf669cff6c43c48248c070c9dff4ecfef979
Atul Jairam <shadesofaura@gmail.com>
2022-02-26 01:45:06 -0500

    Completed the Initial setup of the exercise-associative-array.php

---
+++ Associative Array Manipulation/exercise-associative-array.php
@@ -0,0 +1,39 @@
+<?php
+
+function showTitle($title)
+{
+    echo "<br/><b>&#9830; $title</b><br/>";
+    echo '<hr/>';
+}
+
+$users = array(
+    array(
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ),
+    array(
+        'id' => 3245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith',
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones',
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe',
+    ),
+);
+
+showTitle("Exercise 1 (Show first names without iteration)");
+
+showTitle('Exercise 2 (Show the value of the different element between the two collections $firstArray and $secondArray without iterating)');
+
+$firstArray = array('a' => 'auto', 'b' => 'moto', 'c' => 'airplane');
+$secondArray = array('a' => 'auto', 'b' => 'moto');
+
+showTitle('Exercise 3 (Reverse the keys and values of the $firstArray array without iterating)');
