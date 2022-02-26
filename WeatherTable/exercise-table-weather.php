8d0fd5f62b79a2b751d77a6f147596b9edc2a7ed
Atul Jairam <shadesofaura@gmail.com>
2022-02-26 01:29:07 -0500

    Completed the exercise-table-weather.php

--- WeatherTable/exercise-table-weather.php
+++ WeatherTable/exercise-table-weather.php
@@ -1,43 +1,12 @@
 <?php
 
-define('IMG_PATH', 'images/');
+define('IMG_PATH', './images/');
 
 define('FILENAME_K', 'filename');
 define('ALT_K', 'alt');
 define('TEMPS_K', 'temps');
 
-$forecasts = array(
-    '2021-04-11' => array(
-        FILENAME_K => 'clearance.gif',
-        ALT_K => 'clearance image',
-        TEMPS_K => '-17ºC',
-    ),
-    '2021-04-12' => array(
-        FILENAME_K => 'sun_cloud.gif',
-        ALT_K => 'sun and cloud image',
-        TEMPS_K => '-11ºC',
-    ),
-    '2021-04-13' => array(
-        FILENAME_K => 'snow.gif',
-        ALT_K => 'snow image',
-        TEMPS_K => '-12ºC',
-    ),
-    '2021-04-14' => array(
-        FILENAME_K => 'sun.gif',
-        ALT_K => 'sun image',
-        TEMPS_K => '-15ºC',
-    ),
-    '2021-04-15' => array(
-        FILENAME_K => 'snow.gif',
-        ALT_K => 'snow image',
-        TEMPS_K => '-11ºC',
-    ),
-    '2021-04-16' => array(
-        FILENAME_K => 'sun.gif',
-        ALT_K => 'sun image',
-        TEMPS_K => '-15ºC',
-    ),
-);
+$forecasts = array('2021-04-11' => array(FILENAME_K => 'clearance.gif', ALT_K => 'clearance image', TEMPS_K => '-17ºC',), '2021-04-12' => array(FILENAME_K => 'sun_cloud.gif', ALT_K => 'sun and cloud image', TEMPS_K => '-11ºC',), '2021-04-13' => array(FILENAME_K => 'snow.gif', ALT_K => 'snow image', TEMPS_K => '-12ºC',), '2021-04-14' => array(FILENAME_K => 'sun.gif', ALT_K => 'sun image', TEMPS_K => '-15ºC',), '2021-04-15' => array(FILENAME_K => 'snow.gif', ALT_K => 'snow image', TEMPS_K => '-11ºC',), '2021-04-16' => array(FILENAME_K => 'sun.gif', ALT_K => 'sun image', TEMPS_K => '-15ºC',),);
 
 ?>
 
@@ -47,6 +16,11 @@ $forecasts = array(
 <head>
     <meta charset="utf-8"/>
     <title>Weather Table</title>
+    <style>
+        span {
+            font-weight: bold;
+        }
+    </style>
 </head>
 
 <body>
@@ -56,6 +30,35 @@ $forecasts = array(
     </header>
     <main>
         <table>
+            <thead></thead>
+            <tbody>
+            <tr>
+                <td>
+
+
+                    <?php
+                    foreach ($forecasts as $date => $forecast) {
+                        //var_dump($forecast[FILENAME_K]);
+                        ?>
+
+
+                        <div>
+                            <span><?= $date ?></span>
+                            <img src="<?= IMG_PATH . $forecast[FILENAME_K] ?>" alt="<?= $forecast[ALT_K] ?>">
+                            <span><?= $forecast[TEMPS_K] ?></span>
+                        </div>
+                        <br>
+
+
+                        <?php
+
+                    } ?>
+                </td>
+            </tr>
+
+            </tbody>
+
+
         </table>
     </main>
 </div>
