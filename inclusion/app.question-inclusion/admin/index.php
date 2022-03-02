13890b7b6bcb3253df934233225c0981db0cc9c2
Atul Jairam <shadesofaura@gmail.com>
2022-03-02 13:21:55 -0500

    completed the exercise

---
+++ inclusion/app.question-inclusion/admin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+    require_once "../include/head.php"
+    ?>
+</head>
+
+<body>
+<?php
+require_once "./include/admin-navbar.php"
+?>
+
+<div class="container-fluid">
+    <p class="page-content">Home administration</p>
+</div>
+
+<?php
+require_once "../include/footer.php"
+?>
+</body>
+
+</html>
