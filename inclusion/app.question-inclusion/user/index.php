890eacc1974e61abaf07a2f27ea598867ab4bb55
Atul Jairam <87813463+WhiteFang465@users.noreply.github.com>
2022-03-02 14:15:08 -0500

    completed the exercise (#5)

---
+++ inclusion/app.question-inclusion/user/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+    require_once "../include/head.php";
+    ?>
+</head>
+
+<body>
+<?php
+require_once "./../user/include/user-navbar.php";
+?>
+
+<div class="container-fluid">
+    <p class="page-content">User home</p>
+</div>
+
+<?php
+require_once "../include/footer.php";
+?>
+</body>
+
+</html>
