ab632ee8b54c8195b60a24dd8511703b1479258a
Atul Jairam <shadesofaura@gmail.com>
2022-03-03 17:10:43 -0500

    Final UI changes done

--- FormBuilding/exercise-form-w3CSS/exercise-Form.php
+++ FormBuilding/exercise-form-w3CSS/exercise-Form.php
@@ -2,7 +2,7 @@
 
 //Not the idealist associative array but one required for learning. DO NOT MODIFY
 $forms = [
-    ["action" => "exercise-Form.php", "method" => "post"],
+    ["action" => "management.php", "method" => "post"],
     ["type" => "text", "id" => "lastName", "name" => "lastName", "label" => "Last Name"],
     ["type" => "text", "id" => "firstName", "name" => "firstName", "label" => "First Name"],
     ["type" => "password", "id" => "pwd", "name" => "pwd", "label" => "Password"],
@@ -22,14 +22,31 @@ $forms = [
     <h2>My Form</h2>
 </div>
 
-<form class="w3-container" method="<?= $forms[0]["method"] ?>" action="<?= $forms[0]["action"] ?>" >
+<form class="w3-container w3-grey" method="<?= $forms[0]["method"] ?>" action="<?= $forms[0]["action"] ?>" >
     <?php
 for ($position=1; $position< count($forms); $position++)
 {
 ?>
+
     <p>
+        <?php
+        if ($forms[$position]["type"]=="submit"){
+
+
+        ?>
+                <button class="w3-button w3-block w3-blue " type="<?= $forms[6]["type"]?>" id="<?= $forms[6]["id"] ?> " name="<?=$forms[6]["name"] ?>">
+<?= $forms[6]["type"] ?>
+                </button>
+
+        <?php
+        }
+else{
+        ?>
         <label><?= $forms[$position]["label"] ?></label>
         <input class="w3-input" type="<?= $forms[$position]["type"]?>" id="<?= $forms[$position]["id"] ?> " name="<?=$forms[$position]["name"] ?>"></p>
+    <?php
+    }
+    ?>
 <?php
 }
 ?>
